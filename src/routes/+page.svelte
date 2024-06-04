55f0f35de9517e737b8e4870820bdd03e5b3725a
leekeh <71131586+leekeh@users.noreply.github.com>
2024-06-04 23:12:37 +0200

    feat: update ui logic and start using state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,49 @@
 <script lang="ts">
 	import type { ChangeEventHandler } from 'svelte/elements';
-	export let data;
+	import { getRestaurants } from 'api';
+	let { data } = $props();
 
-	let selectedCuisineId = 0;
-	let selectedCity = data.clientLocation.city;
+	let selectedCuisineId = $state(0);
+	let selectedCity = $state(data.clientLocation.city);
+	let restaurants = $state(data.restaurants);
 	let selectedCoordinates = data.clientLocation.coordinates;
 
 	function lookupLocation(evt: Parameters<ChangeEventHandler<HTMLInputElement>>['0']) {
 		//todo
+	}
+
+	async function updateRestaurants() {
+		restaurants = await getRestaurants({ ...selectedCoordinates, cuisineIds: [selectedCuisineId] });
 	}
 </script>
 
 <h1>Polynom</h1>
 
 <div>
-	I am looking for <select bind:value={selectedCuisineId} name="cuisine">
-		<option value="0" disabled>Cuisine</option>
+	I am looking for <select
+		bind:value={selectedCuisineId}
+		onchange={updateRestaurants}
+		name="cuisine"
+	>
+		<option value={0}>international</option>
 		<hr />
-		{#each data.availableCuisines ?? [] as cuisine}
+		{#each data.availableCuisines as cuisine}
 			<option value={cuisine.id}>{cuisine.name}</option>
 		{/each}
 		<hr />
 		<option disabled>More options coming soon!</option>
 	</select>
-	cuisine near <input bind:value={selectedCity} on:change={lookupLocation} type="text" />
+	cuisine near <input bind:value={selectedCity} onchange={lookupLocation} type="text" />
 </div>
 
-{#each data.restaurants.data as restaurant}
-	<div>
-		<h2>{restaurant.name}</h2>
-	</div>
-{/each}
-
-test: {selectedCuisineId}
+{#if restaurants.length === 0}
+	<p>No restaurants were found. If you have any recommendations, please let us know!</p>
+{:else}
+	<ul>
+		{#each restaurants as restaurant}
+			<li>
+				<h2>{restaurant.name}</h2>
+			</li>
+		{/each}
+	</ul>
+{/if}
