78b0feb5e9de8a7d8bd8b8f694e46a6d939b3f13
leekeh <71131586+leekeh@users.noreply.github.com>
2024-07-04 18:45:41 +0200

    chore: cleanup

--- src/lib/api/supabase/cuisines.ts
+++ src/lib/api/supabase/cuisines.ts
@@ -1,10 +1,6 @@
 import { supabase } from 'db';
 import { useSupabase, type Config } from './util.svelte';
 
-const myfunct = () => console.log('hiiii');
-
-globalThis.myFunct = myfunct;
-
 export function getAvailableCuisines(config?: Config) {
 	return useSupabase(async () => await supabase.from('cuisines').select('name, id'), config);
 }
