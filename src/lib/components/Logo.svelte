d8b72af88f9215c6862dc7aaa781f43708c3257c
leekeh <71131586+leekeh@users.noreply.github.com>
2024-08-15 20:18:35 +0200

    feat: improve logo

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,10 +1,19 @@
-<p>Nomdial</p>
+<script>
+	import { Card } from 'components';
+	import { page } from '$app/stores';
+</script>
+
+<Card hasBorder style="width: fit-content;">
+	{#if $page.url.pathname !== '/'}
+		<a class="logo" href="/" aria-label="Homepage">Nom·dial</a>
+	{:else}
+		<p class="logo">Nom·dial</p>
+	{/if}
+</Card>
 
 <style>
-	p {
-		background-color: rgb(255, 254, 253);
-		font-family: OldNewspaperTypes;
-		width: fit-content;
-		padding: 1rem;
+	.logo {
+		font-size: 1.8rem;
+		margin: 0 2rem;
 	}
 </style>
